6ec8f0cf6fd156c1750bf02ae6d96128adb0f815
KazuyaMakino <101450293+Glen-Maki@users.noreply.github.com>
2022-06-18 13:55:58 +0900

    [追加] カメラ画面の実装・CameraPage.tsx

---
+++ FaceApp/src/Page/CameraPage.tsx
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from "react";
+import { Text, View, TouchableOpacity, StyleSheet } from "react-native";
+import { Camera, CameraType } from "expo-camera";
+
+export const App = () => {
+  const [hasPermission, setHasPermission] = useState<null | boolean>(null);
+  const [type, setType] = useState(CameraType.back);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+  return (
+    <View style={styles.container}>
+      <Camera style={styles.camera} type={type}>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => {
+              setType(
+                type === CameraType.back ? CameraType.front : CameraType.back
+              );
+            }}
+          >
+            <Text style={styles.text}> Flip </Text>
+          </TouchableOpacity>
+        </View>
+      </Camera>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+  },
+  button: {
+    flex: 1,
+  },
+  text: {
+    flex: 1,
+  },
+});
