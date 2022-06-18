2305e69f10b9ce24c6c8aed928493cf13b8f1365
KazuyaMakino <101450293+Glen-Maki@users.noreply.github.com>
2022-06-18 14:21:37 +0900

    [修正] Navition設定・Home.tsx/App.tsx

--- FaceApp/src/App.tsx
+++ FaceApp/src/App.tsx
@@ -4,20 +4,22 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import { Home } from "./Page/Home";
+import { CameraPage } from "./Page/CameraPage";
 
-type RootStackParamList = {
+/* type RootStackParamList = {
   Home: undefined;
   Profile: { userId: string };
   Feed: { sort: "latest" | "top" } | undefined;
-};
+}; */
 
-const Stack = createNativeStackNavigator<RootStackParamList>();
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="CameraPage" component={CameraPage} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- FaceApp/src/Page/Home.tsx
+++ FaceApp/src/Page/Home.tsx
@@ -1,10 +1,13 @@
 import { View, Text, Button } from "react-native";
 
-export const Home = () => {
+export const Home = ({ navigation }: any) => {
   return (
     <View>
       {/* onPress={() => navigation.navigate('camera')}を追加 */}
-      <Button title="診断を始める" />
+      <Button
+        title="診断を始める"
+        onPress={() => navigation.navigate("CameraPage")}
+      />
     </View>
   );
 };
