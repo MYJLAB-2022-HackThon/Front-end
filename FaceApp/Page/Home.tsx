f01488a75a0399c9f0fde2c7155bf2904ba2ea7c
KazuyaMakino <101450293+Glen-Maki@users.noreply.github.com>
2022-06-18 10:17:50 +0900

    [追加] ホーム画面追加・App.tsx
    ボタンに画面遷移機能を追加予定

---
+++ FaceApp/Page/Home.tsx
@@ -0,0 +1,10 @@
+import { View, Text, Button } from "react-native";
+
+export const Home = () => {
+  return (
+    <View>
+      {/* onPress={() => navigation.navigate('camera')}を追加 */}
+      <Button title="診断を始める" />
+    </View>
+  );
+};
